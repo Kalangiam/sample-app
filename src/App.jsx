458cc5d1ba90f046ffed0675d940ac84e8dc0916
Kalangiam <gladsonkalangiam@gmail.com>
2024-10-17 21:18:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ErrorBoundary from './components/ErrorBoundary'
+function App() {
+  return (
+    <ErrorBoundary>
+    </ErrorBoundary>
+  )
+}
+
+export default App
---
+++ src/utils/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import Home from "../components/Home";
+import Login from "../components/Login";
+
+const AppRouter = [
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <Login /> }
+]
+
+
+export default AppRouter
